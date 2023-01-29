8fc30d00ebcea307b8c1c45b37d55242d5bfdfe5
Santiago Mena <santiagomenape@gmail.com>
2023-01-29 19:13:38 -0300

    3. Introducción a los objetos

---
+++ 3.Introduccion_a_los_objetos.py
@@ -0,0 +1,5 @@
+peso = float(input('Ingrese su peso en kg: '))
+estatura = float(input('Ingrese su estatura en mts: '))
+indice = peso / (estatura**2)
+print('Su indice de masa corporal es: '+str(indice))
+
