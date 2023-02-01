7022f48500f2e3d3d4a952f32c9b8f8b6f33f8c4
Santiago Mena <santiagomenape@gmail.com>
2023-02-01 19:07:10 -0300

    12. Ejercicio12: Init Django + Models (Director & Pelicula)

---
+++ 12.Django/ejercicio12/directores/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.urls import reverse
+import uuid
+# Create your models here.
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return '%s, %s' % (self.last_name, self.first_name)
+
+    def get_absolute_url(self):
+        return reverse('director-detail', args=[str(self.id)])
+
+class Pelicula(models.Model):
+    title = models.CharField(max_length=32)
+    director = models.ForeignKey('Director', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=100, help_text='Describe la pelicula')
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('pelicula-detail', args=[str(self.id)])
