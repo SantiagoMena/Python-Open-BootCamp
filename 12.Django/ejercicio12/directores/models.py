228dbb49394a6811370d72c9c550b6b1199fd4b4
Santiago Mena <santiagomenape@gmail.com>
2023-02-02 01:35:41 -0300

    12.Django: Ejercicio Final

--- 12.Django/ejercicio12/directores/models.py
+++ 12.Django/ejercicio12/directores/models.py
@@ -3,16 +3,6 @@ from django.urls import reverse
 import uuid
 # Create your models here.
 
-class Director(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    def __str__(self) -> str:
-        return '%s, %s' % (self.last_name, self.first_name)
-
-    def get_absolute_url(self):
-        return reverse('director-detail', args=[str(self.id)])
-
 class Pelicula(models.Model):
     title = models.CharField(max_length=32)
     director = models.ForeignKey('Director', on_delete=models.SET_NULL, null=True)
@@ -23,3 +13,14 @@ class Pelicula(models.Model):
 
     def get_absolute_url(self):
         return reverse('pelicula-detail', args=[str(self.id)])
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    peliculas = models.ManyToOneRel('director', Pelicula, 'director')
+
+    def __str__(self) -> str:
+        return '%s, %s' % (self.last_name, self.first_name)
+
+    def get_absolute_url(self):
+        return reverse('director-detail', args=[str(self.id)])
--- 12.Django/ejercicio12/directores/views.py
+++ 12.Django/ejercicio12/directores/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Director, Pelicula
 # Create your views here.
+def index(request):
+    directores = Director.objects.all()
+    data = []
+    for director in directores:
+        peliculas = Pelicula.objects.filter(director=director)
+        data.append({"nombre": director.first_name + ' ' + director.last_name, "peliculas": peliculas})
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'data': data,
+        }
+    )
+
+    
